ed13d7557b5dd318f1ecbfed2928fdf858c453cd
iamchrispham <chrismpham@gmail.com>
2019-06-07 19:50:29 -0700

    fixes style

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -2,7 +2,7 @@ var Messages = {
   allMessages: [],
 };
 
-var Message = function (text){
+var Message = function (text) {
   this.text = text;
   this.username = App.username;
   this.roomname = Rooms.current;
